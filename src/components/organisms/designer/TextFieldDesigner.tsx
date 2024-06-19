906273bb39c73897901b84d55e8241f580cf73a2
ajeetsingh22 <ajeet.singh@rankbit.tech>
2024-06-19 18:06:30 +0530

    new property add

--- src/components/organisms/designer/TextFieldDesigner.tsx
+++ src/components/organisms/designer/TextFieldDesigner.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 interface TextFieldDesignerProps {
-    label: string
-    placeholder: string
-    name: string
+    label: string;
+    placeholder: string;
+    name: string;
 }
 
-const TextFieldDesigner = ({ label, placeholder, name }: TextFieldDesignerProps) => {
+const TextFieldDesigner: React.FC<TextFieldDesignerProps> = ({ label, placeholder, name }) => {
     return (
         <div className="p-2 my-1 border rounded bg-white shadow">
             <label>{label}</label>
-            <input readOnly disabled placeholder={placeholder} type="text" className="mt-1 p-2 border rounded w-full" />
+            <input readOnly disabled placeholder={placeholder} type="text" name={name} className="mt-1 p-2 border rounded w-full" />
         </div>
     );
 };
