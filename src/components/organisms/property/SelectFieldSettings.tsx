316bae282ffd2ff680a3aa225dd12407c35aacf8
= <=>
2024-06-27 18:39:56 +0530

    select field option

--- src/components/organisms/property/SelectFieldSettings.tsx
+++ src/components/organisms/property/SelectFieldSettings.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
-import { Input, Form } from 'antd';
+import { Input, Form, Button, Row, Col } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import DividerWithHeader from '@components/atoms/Divider';
+import useSettingsForm from '@hooks/useSettingsForm';
 
 const SelectFieldSettings: React.FC = () => {
+    const { handleValuesChange, values } = useSettingsForm();
+
     return (
-        <Form layout="vertical">
-            <Form.Item label="Label">
+        <Form initialValues={values} layout="vertical" onValuesChange={handleValuesChange}>
+            <Form.Item label="Label" name="label">
                 <Input />
             </Form.Item>
-            <Form.Item label="Name">
+            <Form.Item label="Name" name="name">
                 <Input />
             </Form.Item>
             {/* Add more settings specific to select fields */}
+
+            <DividerWithHeader title="Options" />
+            <Form.List name="options">
+                {(fields, { add, remove }) => (
+                    <>
+                        {fields.map(({ key, name, ...restField }) => (
+                            <Row gutter={16} key={key} align="middle">
+                                <Col span={10}>
+                                    <Form.Item
+                                        {...restField}
+                                        name={[name, 'label']}
+                                        label="Label"
+                                        rules={[{ required: true, message: 'Missing option label' }]}
+                                    >
+                                        <Input placeholder="Label" />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={10}>
+                                    <Form.Item
+                                        {...restField}
+                                        name={[name, 'value']}
+                                        label="Value"
+                                        rules={[{ required: true, message: 'Missing option value' }]}
+                                    >
+                                        <Input placeholder="Value" />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={4}>
+                                    <MinusCircleOutlined
+                                        onClick={() => remove(name)}
+                                        style={{ fontSize: '24px', color: 'red' }}
+                                    />
+                                </Col>
+                            </Row>
+                        ))}
+                        <Form.Item>
+                            <Button type="dashed" onClick={() => add({ label: '', value:`` })} icon={<PlusOutlined />}>
+                                Add Option
+                            </Button>
+                        </Form.Item>
+                    </>
+                )}
+            </Form.List>
         </Form>
     );
 };
