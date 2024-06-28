131c0e6a433ee1121c1a1e587be5b6d982e6185e
Pratik Soni <pratik.soni@simformsolutions.com>
2024-06-28 15:50:31 +0530

    REmove unwanted imports

--- src/components/organisms/property/SectionFieldSettings.tsx
+++ src/components/organisms/property/SectionFieldSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, Form, Switch, InputNumber } from 'antd';
+import { Input, Form, Switch } from 'antd';
 import useSettingsForm from '@hooks/useSettingsForm';
 
 const SectionFieldSettings: React.FC = () => {
