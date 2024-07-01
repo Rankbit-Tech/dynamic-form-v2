69b582147048ef67b9cd979c96e571d58c2b47c8
= <=>
2024-07-01 18:43:11 +0530

    Password pattern

--- src/components/organisms/property/PasswordFieldSettings.tsx
+++ src/components/organisms/property/PasswordFieldSettings.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import { Input, Form, InputNumber } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Input, Form, InputNumber, Checkbox } from 'antd';
 import useSettingsForm from '@hooks/useSettingsForm';
 
 const PasswordFieldSettings: React.FC = () => {
-    const { handleValuesChange, values } = useSettingsForm();
+    const { handleValuesChange, values, setValues } = useSettingsForm();
+    const [validationPattern, setValidationPattern] = useState('');
+
+    const updateValidationRules = (pattern) => {
+        let Pattern = '';
+        if (pattern.includes('number')) {
+            Pattern += '(?=.*[0-9])';
+        }
+        if (pattern.includes('alphabet')) {
+            Pattern += '(?=.*[a-zA-Z])';
+        }
+        if (pattern.includes('specialCharacter')) {
+            Pattern += '(?=.*[!@#$%^&*])';
+        }
+        setValidationPattern(Pattern);
+    };
+
+    useEffect(() => {
+        if (values.pattern) {
+            updateValidationRules(values.pattern);
+        }
+    }, [values.pattern]);
 
     if (!values) {
-        return null; 
+        return null;
     }
 
     return (
-        <Form layout="vertical" initialValues={values} onValuesChange={handleValuesChange}>
+        <Form
+            layout="vertical"
+            initialValues={values}
+            onValuesChange={(changedValues, allValues) => {
+                handleValuesChange(changedValues, allValues);
+                if (changedValues.pattern) {
+                    updateValidationRules(changedValues.pattern);
+                }
+            }}
+        >
             <Form.Item label="Label" name="label">
                 <Input />
             </Form.Item>
             <Form.Item label="Name" name="name">
                 <Input />
             </Form.Item>
-            <Form.Item label="Default Value" name="defaultValue">
+            <Form.Item
+                label="Default Value"
+                name="defaultValue"
+                rules={[
+                    {
+                        pattern: new RegExp(validationPattern),
+                        message: 'Password does not meet the required criteria',
+                    },
+                ]}
+            >
                 <Input.Password />
             </Form.Item>
             <Form.Item label="Min Length" name="minLength">
@@ -27,7 +66,11 @@ const PasswordFieldSettings: React.FC = () => {
                 <InputNumber min={0} />
             </Form.Item>
             <Form.Item label="Pattern" name="pattern">
-                <Input />
+                <Checkbox.Group>
+                    <Checkbox value="number">Number</Checkbox>
+                    <Checkbox value="alphabet">Alphabet</Checkbox>
+                    <Checkbox value="specialCharacter">Special Character</Checkbox>
+                </Checkbox.Group>
             </Form.Item>
         </Form>
     );
