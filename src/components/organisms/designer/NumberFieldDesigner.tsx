881ffb820370ad10060110465896f14dc89442be
Pratik Soni <pratik.soni@simformsolutions.com>
2024-07-02 13:25:41 +0530

    add validation

--- src/components/organisms/designer/NumberFieldDesigner.tsx
+++ src/components/organisms/designer/NumberFieldDesigner.tsx
@@ -1,11 +1,12 @@
+import { InputNumber } from 'antd';
 import React from 'react';
 
-const NumberFieldDesigner: React.FC<{ label: string }> = ({ label }) => {
+const NumberFieldDesigner: React.FC<{ label: string, placeholder: string }> = ({ label, placeholder }) => {
     return (
         <div className="p-2 border rounded bg-white shadow">
             <div className="mt-5">
                 <label>{label}</label>
-                <input type="number" className="border p-1 rounded w-full" />
+                <InputNumber placeholder={placeholder} className="border p-1 rounded w-full" />
             </div>
         </div>
     );
