a5ed40c59118324a9e39a76b5d2c8b061648e572
= <=>
2024-07-03 18:31:26 +0530

    Add PasswordFieldSettings component and toggle visibility

--- src/components/organisms/property/FileUploadFieldSettings.tsx
+++ src/components/organisms/property/FileUploadFieldSettings.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Input, Form, Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import useSettingsForm from '@hooks/useSettingsForm';
 
 const FileUploadFieldSettings: React.FC = () => {
+const { handleValuesChange, values } = useSettingsForm();
+
     return (
-        <Form layout="vertical">
-            <Form.Item label="Label">
+        <Form layout="vertical" initialValues={values} onValuesChange={handleValuesChange}>
+            <Form.Item label="Label" name="label">
                 <Input />
             </Form.Item>
-            <Form.Item label="Name">
+            <Form.Item label="Name" name="name">
                 <Input />
             </Form.Item>
 
-            <Form.Item label="Accepted File Types">
+            <Form.Item label="Accepted File Types" name="accepted file types">
                 <Input placeholder="e.g., .jpg, .png, .pdf" />
             </Form.Item>
-            <Form.Item label="Max File Size (MB)">
+            <Form.Item label="Max File Size (MB)" name="max file size (MB)">
                 <Input type="number" placeholder="e.g., 5" />
             </Form.Item>
-            <Form.Item label="Default File">
+            <Form.Item label="Default File" name="default file">
                 <Upload>
                     <Button icon={<UploadOutlined />}>Upload</Button>
                 </Upload>
