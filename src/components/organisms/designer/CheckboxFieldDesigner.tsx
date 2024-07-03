86bfdc87701b63ee2d41cdb8102ae224d2189e9e
ajeetsingh22 <ajeet.singh@rankbit.tech>
2024-07-03 17:29:16 +0530

    adding-top-bottom-area field

--- src/components/organisms/designer/CheckboxFieldDesigner.tsx
+++ src/components/organisms/designer/CheckboxFieldDesigner.tsx
@@ -1,22 +1,55 @@
+import DragAreaSeperator from '@components/atoms/DragAreaSeperator';
+import DragAreaSplitter from '@components/atoms/DragAreaSplitter';
+import { VARIANT } from '@constants/fieldTypes';
+import { useDroppable } from '@dnd-kit/core';
 import { Checkbox } from 'antd';
 import React from 'react';
 
 interface CheckboxFieldDesignerProps {
     label: string;
     options?: string[];
+    id: string
+    parentId: string
+
+
 }
 
-const CheckboxFieldDesigner: React.FC<CheckboxFieldDesignerProps> = ({ label, options }) => {
+const CheckboxFieldDesigner: React.FC<CheckboxFieldDesignerProps> = ({ label, options, parentId, id }) => {
+    const topHalf = useDroppable({
+        id: `top-field-${id}`,
+        data: {
+            id: id,
+            position: "top",
+            parentId,
+            type: VARIANT.FIELD
+        }
+
+    });
+    const bottomHalf = useDroppable({
+        id: `bottom-field-${id}`,
+        data: {
+            id: id,
+            position: "bottom",
+            parentId,
+            type: VARIANT.FIELD
+        }
+    });
 
     console.log({ label })
     return (
-        <div className="p-2 py-4 border rounded bg-white shadow">
-            <label>{label}</label>
-            <div className="mt-1">
-                <Checkbox.Group
-                    options={options}
-                />
-            </div>
+        <div className=" py-4 border rounded bg-white shadow">
+            <DragAreaSplitter topRef={topHalf.setNodeRef} bottomRef={bottomHalf.setNodeRef} />
+            <DragAreaSeperator topHalf={topHalf} bottomHalf={bottomHalf}>
+                <div className='mt-2 p-2'>
+
+                    <label>{label}</label>
+                    <div className="mt-1">
+                        <Checkbox.Group
+                            options={options}
+                        />
+                    </div>
+                </div>
+            </DragAreaSeperator>
         </div>
     );
 };
--- src/components/organisms/designer/GridFieldDesigner.tsx
+++ src/components/organisms/designer/GridFieldDesigner.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { fieldTypes } from '@constants/fieldTypes';
+import { VARIANT, fieldTypes } from '@constants/fieldTypes';
 import { useDroppable } from '@dnd-kit/core';
 import { cn } from '@utils/cn';
+import DragAreaSeperator from '@components/atoms/DragAreaSeperator';
+import DragAreaSplitter from '@components/atoms/DragAreaSplitter';
 
 interface GridFieldDesignerProps {
     id: string
     type: Pick<typeof fieldTypes, "GRID">
     children: React.ReactNode
     cols: number
+    parentId: string
 }
 
 
-const GridFieldDesigner: React.FC<GridFieldDesignerProps> = ({ id, type, children, cols }) => {
+const GridFieldDesigner: React.FC<GridFieldDesignerProps> = ({ id, type, children, cols, parentId }) => {
 
     const { isOver, setNodeRef } = useDroppable({
         id,
@@ -21,14 +24,39 @@ const GridFieldDesigner: React.FC<GridFieldDesignerProps> = ({ id, type, childre
             parentId: id
         }
     });
+    const topHalf = useDroppable({
+        id: `top-field-${id}`,
+        data: {
+            id: id,
+            position: "top",
+            parentId,
+            type: VARIANT.FIELD
+        }
+
+    });
+    const bottomHalf = useDroppable({
+        id: `bottom-field-${id}`,
+        data: {
+            id: id,
+            position: "bottom",
+            parentId,
+            type: VARIANT.FIELD
+        }
+    });
 
     return (
-        <div className="border border-gray-400 h-full p-2 flex-1 my-2 min-h-[50px] flex gap-2">
-            <div ref={setNodeRef} className={cn(`p-5 w-full mt-4 grid grid-cols-2 gap-2`, {
-                'bg-blue-200': isOver
-            })} style={{ display: "grid", gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}>
-                {children}
-            </div>
+        <div className="border rounded border-gray-400 h-full  flex-1 my-2 min-h-[50px] flex gap-2">
+            <DragAreaSplitter topRef={topHalf.setNodeRef} bottomRef={bottomHalf.setNodeRef} />
+            <DragAreaSeperator topHalf={topHalf} bottomHalf={bottomHalf}>
+
+                <div className='mt-2 p-2'>
+                    <div ref={setNodeRef} className={cn(`p-5 w-full mt-4 grid grid-cols-2 gap-2`, {
+                        'bg-blue-200': isOver
+                    })} style={{ display: "grid", gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}>
+                        {children}
+                    </div>
+                </div>
+            </DragAreaSeperator>
         </div>
     );
 
