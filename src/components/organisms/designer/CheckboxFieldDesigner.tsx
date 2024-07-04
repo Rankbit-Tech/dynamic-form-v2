15b89763a89f04939505fb7046a6d1a931f10eb7
Pratik Soni <pratik.soni@simformsolutions.com>
2024-07-04 16:22:43 +0530

    add margin to label

--- src/components/organisms/designer/CheckboxFieldDesigner.tsx
+++ src/components/organisms/designer/CheckboxFieldDesigner.tsx
@@ -39,8 +39,8 @@ const CheckboxFieldDesigner: React.FC<CheckboxFieldDesignerProps> = ({ label, op
         <div className=" py-4 border rounded bg-white shadow mt-1">
             <DragAreaSplitter topRef={topHalf.setNodeRef} bottomRef={bottomHalf.setNodeRef} />
             <DragAreaSeperator topHalf={topHalf} bottomHalf={bottomHalf}>
-                <div className='mt-2 p-2'>
-                    <label className='mr-2'>{label}</label>
+                <div className='mt-2 p-2 flex flex-col'>
+                    <label className='mr-2 font-semibold mb-1'>{label}</label>
                     <Checkbox.Group
                         options={options}
                     />
