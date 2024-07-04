61ab10fb3208c7bf956c1995b20501e495ba6f82
= <=>
2024-07-04 16:18:41 +0530

    TextField Component Added

---
+++ src/components/organisms/property/TextFieldSettings.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form, Input } from 'antd';
+import useSettingsForm from '@hooks/useSettingsForm';
+import TextArea from 'antd/es/input/TextArea';
+
+const TextFieldSettings: React.FC = () => {
+ const { handleValuesChange, values } = useSettingsForm();
+
+  return (
+    <Form layout="vertical" initialValues={values} onValuesChange={handleValuesChange}>
+            <Form.Item label="Label" name="label">
+                <Input />
+            </Form.Item>
+            <Form.Item label="Paragraph" name="paragraph">
+                <TextArea rows={4} />
+            </Form.Item>
+    </Form>
+  )
+}
+
+export default TextFieldSettings;
