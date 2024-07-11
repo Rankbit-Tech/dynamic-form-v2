d12c123136fbd617ce4ef69b8193ed2470b93d45
Pratik Soni <pratik.soni@simformsolutions.com>
2024-07-11 18:54:44 +0530

    bug fix

--- src/hooks/useEventBus.tsx
+++ src/hooks/useEventBus.tsx
@@ -7,19 +7,20 @@ type EventBusType = {
 
 const eventBus: EventBusType = {};
 
-const getEventBus = (eventName: string): Subject<any> => {
-    if (!eventBus[eventName]) {
-        eventBus[eventName] = new Subject<any>();
-    }
-    return eventBus[eventName];
-};
-
-const emitEvent = (eventName: string, data: any) => {
-    const eventBusInstance = getEventBus(eventName);
-    eventBusInstance.next(data);
-};
-
 const useEventBus = () => {
+
+    const getEventBus = (eventName: string): Subject<any> => {
+        if (!eventBus[eventName]) {
+            eventBus[eventName] = new Subject<any>();
+        }
+        return eventBus[eventName];
+    };
+
+    const emitEvent = (eventName: string, data: any) => {
+        const eventBusInstance = getEventBus(eventName);
+        eventBusInstance.next(data);
+    };
+
     const subscribe = useCallback((eventName: string, callback: (data: any) => void) => {
         const eventBusInstance = getEventBus(eventName);
         const subscription = eventBusInstance.subscribe(callback);
