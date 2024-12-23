7ba4d31452ade1ac538cb62392c6edd1004d8ef6
Ramesh Desai <desairamesh955@gmail.com>
2024-12-24 00:25:08 +0530

    FIx files upload other settings

--- src/components/organisms/property/NumberFieldSettings.tsx
+++ src/components/organisms/property/NumberFieldSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, Form, InputNumber, Row, Col, Checkbox } from 'antd';
+import { Input, Form, InputNumber, Row, Col, Checkbox, Flex } from 'antd';
 import useSettingsForm from '@hooks/useSettingsForm';
 import DividerWithHeader from '@components/atoms/Divider';
 import QueryBuilderComponent from '@components/molecules/QueryBuilder';
@@ -23,9 +23,14 @@ const NumberFieldSettings: React.FC = () => {
 
             <DividerWithHeader title='Validations' />
 
-            <Form.Item initialValue={values?.validations.required} name={['validations', 'required']} valuePropName="checked">
-                <Checkbox>Required</Checkbox>
-            </Form.Item>
+            <Flex justify='space-between'>
+                <Form.Item initialValue={values?.validations.required} name={['validations', 'required']} valuePropName="checked">
+                    <Checkbox>Required</Checkbox>
+                </Form.Item>
+                <Form.Item initialValue={values?.validations.disabled} name={['validations', 'disabled']} valuePropName="checked">
+                    <Checkbox>Disable</Checkbox>
+                </Form.Item>
+            </Flex>
             <Form.Item initialValue={values?.validations.showControls} name="showControls" valuePropName="checked">
                 <Checkbox checked={values?.validations.showControls}>Show Controls (Up and Down arrow)</Checkbox>
             </Form.Item>
