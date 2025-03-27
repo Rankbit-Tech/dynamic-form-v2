a991fcf90dcc087309e74b47db485b89a5bf9adc
Desai Ramesh <desairamesh955@gmail.com>
2025-03-28 03:18:16 +0530

    Add summary only on preview

--- src/pages/Renderer.tsx
+++ src/pages/Renderer.tsx
@@ -2,24 +2,22 @@ import Preview from "@components/organisms/Preview";
 import { FormConfig, useFormStore } from "@store/useFormStore";
 import { transformData } from "@utils/transform";
 import { FormInstance } from "antd";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import _ from "lodash";
 
 type RendererProps = {
   formConfig?: FormConfig;
-  onFormSubmit?: (formData: FormData, form?: FormInstance) => void
-  isPreview?: boolean
+  onFormSubmit?: (formData: FormData, form?: FormInstance) => void;
+  isPreview?: boolean;
 };
 
 const Renderer = ({ formConfig, onFormSubmit, isPreview }: RendererProps) => {
-
   const [fields, setFormConfig] = useFormStore((state) => [
     state.fields,
     state.setFormConfig,
   ]);
 
   const formData = transformData(fields);
-
 
   useEffect(() => {
     if (formConfig) {
@@ -28,12 +26,12 @@ const Renderer = ({ formConfig, onFormSubmit, isPreview }: RendererProps) => {
       if (!_.isEqual(currentConfig, formConfig)) {
         setFormConfig(formConfig);
       }
-
     }
   }, [formConfig, setFormConfig]);
 
-
-  return <Preview data={formData} isPreview={isPreview} onSubmit={onFormSubmit} />;
+  return (
+    <Preview data={formData} isPreview={isPreview} onSubmit={onFormSubmit} />
+  );
 };
 
 export default Renderer;
