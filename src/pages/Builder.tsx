7cb57e1ecd90e826407152fe3a52a328742162a4
Desai Ramesh <desairamesh955@gmail.com>
2025-05-15 18:49:02 +0530

    Remove date format from dayjs

--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -5,7 +5,7 @@ import "../index.css";
 import { FormBuilderPageProps } from "types";
 
 const Builder = ({ onFormSave, isUpdateState }: FormBuilderPageProps) => {
-  console.log("version : 1.4.8");
+  console.log("version : 1.4.9");
   const { isPreview } = useFormStore((state) => ({
     isPreview: state.isPreview,
   }));
