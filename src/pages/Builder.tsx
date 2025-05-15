447573e09bea71ac5f0828b7dc65a365c28ce70b
Desai Ramesh <desairamesh955@gmail.com>
2025-05-15 18:40:22 +0530

    Release new version

--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -5,7 +5,7 @@ import "../index.css";
 import { FormBuilderPageProps } from "types";
 
 const Builder = ({ onFormSave, isUpdateState }: FormBuilderPageProps) => {
-  console.log("version : 1.4.7");
+  console.log("version : 1.4.8");
   const { isPreview } = useFormStore((state) => ({
     isPreview: state.isPreview,
   }));
